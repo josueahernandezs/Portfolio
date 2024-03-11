a5eac1c8e0acc55ba7675eb509ffb9735246d96c
josue hernandez <90362462+josueahernandezs@users.noreply.github.com>
2024-03-11 19:29:04 -0300

    Portafolio

---
+++ portafolio_project/js/animation.js
@@ -0,0 +1,31 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.menu_header' , {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.redes' , {
+        duration: 1000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.acerca_de_mi' , {
+        duration: 1000,
+        origin: 'top',
+        distance: '400px'
+    });
+
+    sr.reveal('.skills' , {
+        duration: 1000,
+        origin: 'top',
+        distance: '10px'
+    });
+
+    sr.reveal('.hobbies' , {
+        duration: 1000,
+        origin: 'top',
+        distance: '10px'
+    });
